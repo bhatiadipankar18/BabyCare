7a061e603d0a5f7bb492f9ef70804489ab191203
yiyayamaya <wuxingjian98@gmail.com>
2022-10-08 14:21:58 -0400

    git init

---
+++ src/main/java/com/ssw555/babycare/BabyCareApplication.java
@@ -0,0 +1,13 @@
+package com.ssw555.babycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssw555/babycare/BabyCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssw555.babycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
