260d49acfbe5f1d8493040a9b08441cfd361ec46
yiyayamaya <wuxingjian98@gmail.com>
2022-12-01 19:50:32 -0500

    add test

--- src/test/java/com/ssw555/babycare/BabyCareApplicationTests.java
+++ src/test/java/com/ssw555/babycare/BabyCareApplicationTests.java
@@ -1,9 +1,11 @@
 package com.ssw555.babycare;
 
+import com.ssw555.babycare.Entity.Child;
 import com.ssw555.babycare.Entity.Result;
 import com.ssw555.babycare.Entity.User;
 import com.ssw555.babycare.JWT.JWTUtils;
 import com.ssw555.babycare.Repo.UserRepository;
+import com.ssw555.babycare.controller.ChildController;
 import com.ssw555.babycare.controller.UserController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -19,6 +23,9 @@ class BabyCareApplicationTests {
 
     @Autowired
     private UserController userController;
+
+    @Autowired
+    private ChildController childController;
 
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
@@ -71,28 +78,22 @@ class BabyCareApplicationTests {
     }
 
 
-//    @Test
-//    void testDuplicatedUserName(){
-//        User user1=new User();
-//        user1.setUsername("parent1");
-//        userController.register(user1, request);
-//        User user2=new User();
-//        user2.setUsername("parent1");
-//        Result<Object> res = userController.register(user2, request);
-//        assertEquals(res.getMsg(), "invalid username");
-//
-//    }
-//
-//    @Test
-//    void testDuplicatedUserName(){
-//        User user1=new User();
-//        user1.setUsername("parent1");
-//        userController.register(user1, request);
-//        User user2=new User();
-//        user2.setUsername("parent1");
-//        Result<Object> res = userController.register(user2, request);
-//        assertEquals(res.getMsg(), "invalid username");
-//
-//    }
+    @Test
+    void testGetChildByParent(){
+
+        List<Child> childByParentIdBefore = childController.getChildByParentId(1);
+        int sizeBefore=childByParentIdBefore.size();
+        Child child1=new Child();
+        child1.setParentId(1);
+        Child child2=new Child();
+        child2.setParentId(1);
+        childController.save(child1);
+        childController.save(child2);
+        List<Child> childByParentIdAfter = childController.getChildByParentId(1);
+        int sizeAfter=childByParentIdAfter.size();
+        assertEquals(sizeAfter-sizeBefore, 2);
+
+    }
+
 
 }
